49fdcae852041450de321a1faef59465010347fe
qraqras <q100rrraaa@gmail.com>
2025-12-25 23:50:37 +0000

    chore: remove legacy all_cops files (replaced by general_config)

--- include/configs/common/all_cops_config.h
+++
@@ -1,1 +0,0 @@
-#error "all_cops_config.h has been removed. Include 'configs/common/general_config.h' and use the new APIs (leuko_general_config_t)."
--- src/configs/common/all_cops_config.c
+++
@@ -1,8 +0,0 @@
-/* Deprecated: implementation moved to src/configs/common/general_config.c */
-#include "configs/common/general_config.h"
-
-/* Old file kept as a compilation stub to allow a clean transition.
- * This file no longer provides any definitions to avoid duplicate
- * symbol issues. Remove this stub once all references to
- * `all_cops_config.c` are gone.
- */
