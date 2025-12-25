767e418a569a056ecf937a79928d6455acbd9bf1
qraqras <q100rrraaa@gmail.com>
2025-12-25 23:49:55 +0000

    chore: physically move AllCops files to general_config and remove shim header; update includes

--- include/configs/common/all_cops_config.h
+++ include/configs/common/all_cops_config.h
@@ -1,14 +1,1 @@
-#ifndef LEUKOCYTE_CONFIGS_ALL_COPS_CONFIG_H
-#define LEUKOCYTE_CONFIGS_ALL_COPS_CONFIG_H
-
-#include "configs/common/general_config.h"
-
-/* Backwards compatibility shim: AllCops -> general */
-typedef leuko_general_config_t leuko_all_cops_config_t;
-
-static inline leuko_all_cops_config_t *leuko_config_get_all_cops_config(leuko_config_t *cfg)
-{
-    return leuko_config_get_general_config(cfg);
-}
-
-#endif /* LEUKOCYTE_CONFIGS_ALL_COPS_CONFIG_H */
+#error "all_cops_config.h has been removed. Include 'configs/common/general_config.h' and use the new APIs (leuko_general_config_t)."
--- src/configs/common/all_cops_config.c
+++ src/configs/common/all_cops_config.c
@@ -1,56 +1,8 @@
-#include <stdlib.h>
-#include <string.h>
+/* Deprecated: implementation moved to src/configs/common/general_config.c */
 #include "configs/common/general_config.h"
 
-leuko_general_config_t *leuko_general_config_initialize(void)
-{
-    leuko_general_config_t *cfg = calloc(1, sizeof(*cfg));
-    if (!cfg)
-        return NULL;
-    cfg->include = NULL;
-    cfg->include_count = 0;
-    cfg->exclude = NULL;
-    cfg->exclude_count = 0;
-
-    cfg->include_re = NULL;
-    cfg->include_re_count = 0;
-    cfg->exclude_re = NULL;
-    cfg->exclude_re_count = 0;
-    return cfg;
-}
-
-void leuko_general_config_free(leuko_general_config_t *cfg)
-{
-    if (!cfg)
-        return;
-    if (cfg->include_re)
-    {
-        for (size_t i = 0; i < cfg->include_re_count; i++)
-            regfree(&cfg->include_re[i]);
-        free(cfg->include_re);
-        cfg->include_re = NULL;
-        cfg->include_re_count = 0;
-    }
-    if (cfg->exclude_re)
-    {
-        for (size_t i = 0; i < cfg->exclude_re_count; i++)
-            regfree(&cfg->exclude_re[i]);
-        free(cfg->exclude_re);
-        cfg->exclude_re = NULL;
-        cfg->exclude_re_count = 0;
-    }
-
-    if (cfg->include)
-    {
-        for (size_t i = 0; i < cfg->include_count; i++)
-            free(cfg->include[i]);
-        free(cfg->include);
-    }
-    if (cfg->exclude)
-    {
-        for (size_t i = 0; i < cfg->exclude_count; i++)
-            free(cfg->exclude[i]);
-        free(cfg->exclude);
-    }
-    free(cfg);
-}
+/* Old file kept as a compilation stub to allow a clean transition.
+ * This file no longer provides any definitions to avoid duplicate
+ * symbol issues. Remove this stub once all references to
+ * `all_cops_config.c` are gone.
+ */
