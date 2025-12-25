5bdf3da3fd5fe389aaf3877b30a6dfc0737eab5f
qraqras <q100rrraaa@gmail.com>
2025-12-25 06:16:19 +0000

    chore: remove legacy YAML headers and stubs; use node/json APIs

--- include/sources/yaml/merge.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEUKOCYTE_SOURCES_YAML_MERGE_H
-#define LEUKOCYTE_SOURCES_YAML_MERGE_H
-
-#include "sources/yaml/node.h"
-
-bool leuko_yaml_merge_nodes(leuko_yaml_node_t *a, leuko_yaml_node_t *b, leuko_yaml_node_t **out);
-
-#endif /* LEUKOCYTE_SOURCES_YAML_MERGE_H */}
